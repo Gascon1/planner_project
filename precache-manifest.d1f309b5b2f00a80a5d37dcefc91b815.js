71e8e4a0e369569420028cc809457ab114c9674b
Gascon1 <mikelgascon@outlook.com>
2020-01-22 17:33:29 -0500

    Updates

---
+++ precache-manifest.d1f309b5b2f00a80a5d37dcefc91b815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7e5c8a60a289eb72da7e20cefe83259",
+    "url": "/planner_project/index.html"
+  },
+  {
+    "revision": "a9a7e7e4d67ee3abd614",
+    "url": "/planner_project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9de457f44d234497ee79",
+    "url": "/planner_project/static/js/2.242acfe8.chunk.js"
+  },
+  {
+    "revision": "84f5adf97a6a466dd50e6b2b7291bba3",
+    "url": "/planner_project/static/js/2.242acfe8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a9a7e7e4d67ee3abd614",
+    "url": "/planner_project/static/js/main.5923f611.chunk.js"
+  },
+  {
+    "revision": "3fa22dabd547da04fdfe",
+    "url": "/planner_project/static/js/runtime-main.f249a3a3.js"
+  }
+]);
