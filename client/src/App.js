31840fc04989544e9ab185feb1f157ae0dc64ada
Gascon1 <mikelgascon@outlook.com>
2020-01-22 14:58:17 -0500

    users can now create, edit and delete rows on the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,119 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MaterialTable from "material-table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        { title: "Name", field: "name" },
+        {
+          title: "Email",
+          field: "email"
+        },
+        {
+          title: "Group",
+          field: "group"
+        }
+      ],
+      data: [
+        {
+          name: "serge",
+          email: "serge@serge.com",
+          group: "losers"
+        },
+        {
+          name: "mikel",
+          email: "mikel@mikel.com",
+          group: "winners"
+        },
+        {
+          name: "serge",
+          email: "serge@serge.com",
+          group: "losers"
+        },
+        {
+          name: "mikel",
+          email: "mikel@mikel.com",
+          group: "winners"
+        },
+        {
+          name: "serge",
+          email: "serge@serge.com",
+          group: "losers"
+        },
+        {
+          name: "mikel",
+          email: "mikel@mikel.com",
+          group: "winners"
+        }
+      ],
+      selectedRow: null
+    };
+  }
+
+  render() {
+    return (
+      <MaterialTable
+        title="Assignment before getting the job"
+        columns={this.state.columns}
+        data={this.state.data}
+        onRowClick={(evt, selectedRow) => this.setState({ selectedRow })}
+        options={{
+          grouping: true,
+          rowStyle: rowData => ({
+            backgroundColor:
+              this.state.selectedRow &&
+              this.state.selectedRow.tableData.id === rowData.tableData.id
+                ? "#52a27e"
+                : "#FFF"
+          })
+        }}
+        editable={{
+          onRowAdd: newData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                this.setState(
+                  prevState => ({
+                    data: [...prevState.data, newData]
+                  }),
+                  resolve
+                );
+              }, 1000);
+            }),
+          onRowUpdate: (newData, oldData) =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                let clone = [];
+                Object.assign(clone, this.state.data);
+                console.log("clone1:", clone);
+                const index = this.state.data.indexOf(oldData);
+                clone[index] = newData;
+                this.setState({ data: clone }, () => {
+                  resolve();
+                });
+              }, 1000);
+            }),
+          onRowDelete: oldData =>
+            new Promise((resolve, reject) => {
+              setTimeout(() => {
+                {
+                  let clone = [];
+                  Object.assign(clone, this.state.data);
+                  console.log("clone1:", clone);
+                  const index = this.state.data.indexOf(oldData);
+                  clone.splice(index, 1);
+                  console.log(clone);
+                  this.setState(
+                    prevState => ({ ...prevState, data: clone }),
+                    resolve
+                  );
+                }
+                resolve();
+              }, 1000);
+            })
+        }}
+      />
+    );
+  }
 }
-
-export default App;
